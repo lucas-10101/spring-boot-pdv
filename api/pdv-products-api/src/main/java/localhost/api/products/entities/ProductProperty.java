50eb6353a5a4beddbb7c41ad2a4039b342f8aa47
lucas-10101 <lucas-quadros@outlook.com.br>
2024-10-21 23:33:52 -0300

    Products API Started
     - added the main classes
     - basic property configuration for development

---
+++ api/pdv-products-api/src/main/java/localhost/api/products/entities/ProductProperty.java
@@ -0,0 +1,41 @@
+package localhost.api.products.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "product_properties")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ProductProperty {
+
+	@EmbeddedId
+	@EqualsAndHashCode.Include
+	private ProductProperty.Identity id;
+
+	@Column(length = 256, nullable = false)
+	private String description;
+
+	@Data
+	@Embeddable
+	public static class Identity implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		@Column(length = 64, nullable = false, updatable = false)
+		private String name;
+
+		@ManyToOne(fetch = FetchType.LAZY, optional = false)
+		@JoinColumn(name = "product_id", nullable = false, updatable = false)
+		private Product product;
+	}
+}
