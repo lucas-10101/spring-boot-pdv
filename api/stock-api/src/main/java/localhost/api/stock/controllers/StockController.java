ab331e33f4201d4c0879e9e9029fe8e8c6888bc1
lucas-10101 <lucas-quadros@outlook.com.br>
2024-12-22 18:15:49 -0300

    Fix stock api 'secured' annotations and role hierarchy

--- api/stock-api/src/main/java/localhost/api/stock/controllers/StockController.java
+++ api/stock-api/src/main/java/localhost/api/stock/controllers/StockController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import localhost.api.stock.services.ProductStockService;
+import localhost.commonslibrary.api.security.Authorities;
 import localhost.modellibrary.api.stock.ProductStockModel;
 
 
@@ -29,6 +31,7 @@ public class StockController {
     @Autowired
     private ModelMapper mapper;
 
+    @Secured(Authorities.StockApi.MANAGE_STOCK)
     @PostMapping("/{productId}")
     public ResponseEntity<Void> createStockForProduct(@PathVariable Integer productId) {
         
@@ -38,6 +41,7 @@ public class StockController {
     }
     
 
+    @Secured(Authorities.StockApi.READ_STOCK)
     @GetMapping(path = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProductStockModel> readStockForProduct(@PathVariable Integer productId) {
 
@@ -46,6 +50,7 @@ public class StockController {
         return ResponseEntity.ok(mapped);
     }
 
+    @Secured(Authorities.StockApi.READ_STOCK)
     @GetMapping()
     public ResponseEntity<Collection<ProductStockModel>> listStock() {
 
@@ -56,12 +61,14 @@ public class StockController {
         return ResponseEntity.ok(list);
     }
 
+    @Secured(Authorities.StockApi.MANAGE_STOCK)
     @PatchMapping(path = "/increase-stock")
     public ResponseEntity<Void> increaseStockQuantityFor(@RequestBody ProductStockModel stockModel) {
         this.productStockService.increaseStockQuantityFor(stockModel.getProductId(), stockModel.getQuantityInStock());
         return ResponseEntity.ok(null);
     }
 
+    @Secured(Authorities.StockApi.MANAGE_STOCK)
     @PatchMapping(path = "/decrease-stock")
     public ResponseEntity<Void> decreaseStockQuantityFor(@RequestBody ProductStockModel stockModel) {
         this.productStockService.decreaseStockQuantityFor(stockModel.getProductId(), stockModel.getQuantityInStock());
