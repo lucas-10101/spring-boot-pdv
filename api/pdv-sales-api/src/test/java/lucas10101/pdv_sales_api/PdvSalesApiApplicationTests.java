e41692881e1a2881b9c4338f804d52ea7bbdfd65
lucas-10101 <lucas-quadros@outlook.com.br>
2024-12-22 14:07:43 -0300

    New project added: sales-api

---
+++ api/pdv-sales-api/src/test/java/lucas10101/pdv_sales_api/PdvSalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lucas10101.pdv_sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvSalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
